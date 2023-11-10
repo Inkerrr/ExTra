80ca6f2fc89c538978fc71b15d729e36aa85b0d4
Inkerrr <artemlysyansky@gmail.com>
2023-11-10 21:02:42 +0200

    50% Completed

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -23,12 +23,33 @@ exports.addIncome = async (req, res) => {
         if(amount <= 0 || !amount === 'number'){ //checks whether the amount is typed
             return res.status(400).json({message: 'Amount must be positive'}) //returns error
         }
+        //await db.Income.save({income})
         await income.save()
-        res.json({message:'Income Added'})
+        res.status(200).json({message:'Income Added'})
         
         } catch (error) {
-        
+            res.status(500).json({message:'Server error', error: error})
     }
 
     console.log(income)
 }
+
+exports.getIncomes = async (req, res) =>{
+    try {
+        const incomes = await IncomeSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.deleteIncome = async (req, res) =>{
+    const{id} = req.params; 
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) =>{
+            res.status(200).json({message: 'Income Deleted'})
+        }) 
+        .catch((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        }) 
+}   
